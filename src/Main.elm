f1f09cef0d571b04d56233130258725a02b3f4a9
Mateusz Czapliński <czapkofan@gmail.com>
2018-05-07 00:23:27 +0200

    initial test of checklist display

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,15 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, p)
+-- import Array exposing (Array)
 -- debois/elm-mdl - Material Design Lite (MDL)
 import Material
 import Material.Scheme
 import Material.Options as Options
+import Material.Color as Color
 import Material.Textfield as Textfield
 import Material.Typography as Typo
+import Material.List as Lists
 import Material.Helpers exposing (pure)
 
 
@@ -15,23 +18,10 @@ import Material.Helpers exposing (pure)
 
 type alias Model =
     { text : String
+    -- , checklists : List Checklist
+    , checklist : Checklist
     , mdl : Material.Model  -- MDL boilerplate
     }
-
-
-model : Model
-model =
-    { text = ""
-    , mdl = Material.model  -- MDL boilerplate
-    }
-
-
-init : ( Model, Cmd Msg )
-init = ( model, Cmd.none )
-
-
-type alias DodoModel =
-    List Checklist
 type alias Checklist =
     { name : String
     , tasks : List Task
@@ -41,21 +31,24 @@ type alias Task =
     , done : Bool
     }
 
+
+model : Model
+model =
+    { text = ""
+    , checklist = Checklist "New List 0"
+        [ Task "Foo" False
+        , Task "Bar" True
+        ]
+    , mdl = Material.model  -- MDL boilerplate
+    }
+
+
+init : ( Model, Cmd Msg )
+init = ( model, Cmd.none )
+
+
 ---- UPDATE ----
 
-
-type Msg
-    = Change String
-    | Mdl (Material.Msg Msg)  -- MDL boilerplate
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Change newText ->
-            pure { model | text = newText }
-        Mdl msg_ ->
-            Material.update Mdl msg_ model
 
 {-
 We need to be able to:
@@ -72,16 +65,51 @@ We need to be able to:
 - TODO: add new list
 - TODO: copy (duplicate) a list with a new name
 -}
-type DodoMsg
-    = LoadFromStorage
-    | AppendTask String
-    | ToggleTask Int
-    | DeleteTask Int
+type Msg
+    = Change String
+    -- = LoadFromStorage
+    -- | AppendTask String
+    -- | ToggleTask Int
+    -- | DeleteTask Int
+    | Mdl (Material.Msg Msg)  -- MDL boilerplate
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Change newText ->
+            pure { model | text = newText }
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
 
 
 ---- VIEW ----
 
 type alias Mdl = Material.Model
+
+viewTask idx submodel =
+    let
+        color =
+            if submodel.done
+            then Color.color Color.Grey Color.S300
+            else Color.black
+    in Lists.li
+        [ ]
+        -- [ Options.css "border-bottom" "1px solid #000 !important"
+        -- ]
+        -- TODO(akavel): somehow add dividers, maybe in similar way as in:
+        -- https://github.com/google/material-design-lite/pull/1785/files
+        [ Lists.content
+            -- [ Color.text (Color.color Color.Grey Color.S300) ]
+            -- [ Color.text Color.black ]
+            [ Color.text color
+            -- , Options.css "border-bottom" "1px solid rgba(#000, 0.12) !important"
+            -- , Options.css "border-bottom" "1px solid #000 !important"
+            ]
+            -- []
+            [ text (submodel.text) ]
+        ]
 
 view : Model -> Html Msg
 view model =
@@ -95,8 +123,8 @@ view model =
         , Options.styled p
             [ Typo.headline ]
             [ text (String.reverse model.text) ]
-        -- [ input [ placeholder "Your text", onInput Change ] []
-        -- , div [] [ text (String.reverse model.text) ]
+        , Lists.ul []
+            (List.indexedMap viewTask model.checklist.tasks)
         ]
     |> Material.Scheme.top
 
