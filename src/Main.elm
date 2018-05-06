ac8522dba2bc3186aeaca450e73c4cd7b2f81482
Mateusz Czapliński <czapkofan@gmail.com>
2018-05-06 22:37:56 +0200

    prepare initial sketch of Model and Msg for the Dodo app

--- src/Main.elm
+++ src/Main.elm
@@ -18,6 +18,16 @@ init =
     ( { text = "" }, Cmd.none )
 
 
+type alias DodoModel =
+    List Checklist
+type alias Checklist =
+    { name : String
+    , tasks : List Task
+    }
+type alias Task =
+    { text : String
+    , done : Bool
+    }
 
 ---- UPDATE ----
 
@@ -33,6 +43,26 @@ update msg model =
             { model | text = newText }
     in ( newModel, Cmd.none )
 
+{-
+We need to be able to:
+- add new items to the current list
+- remove item from current list
+- disable/reenable item from current list (toggle)
+- edit an item on the list
+- TODO: clear (hide) all disabled items
+- TODO: move items around the list
+- TODO: show names of all lists
+- TODO: change the current list to a different one
+- TODO: rename current list
+- TODO: delete a list
+- TODO: add new list
+- TODO: copy (duplicate) a list with a new name
+-}
+type DodoMsg
+    = LoadFromStorage
+    | AppendTask String
+    | ToggleTask Int
+    | DeleteTask Int
 
 
 ---- VIEW ----
